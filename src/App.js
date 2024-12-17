f7d73d6acd61b699bd3a4cd33a82e64ceddc2faa
FramNaVer <139216952+FramNaVer@users.noreply.github.com>
2024-12-18 04:37:35 +0700

    profile nav and aboutme

---
+++ src/App.js
@@ -0,0 +1,21 @@
+//import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home.jsx'
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+
+
+      <main className='main'>
+        <Home />
+      </main>
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+export const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/fram._.0216/" className="home__social-icon" target="_blank">
+                <i class="uil uil-instagram"></i>
+            </a>
+            <a href="https://web.facebook.com/fram.naver" className="home__social-icon" target="_blank">
+                <i class="uil uil-facebook-messenger-alt"></i>
+            </a>
+            <a href="https://github.com/FramNaVer" className="home__social-icon" target="_blank">
+                <i class="uil uil-github"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
